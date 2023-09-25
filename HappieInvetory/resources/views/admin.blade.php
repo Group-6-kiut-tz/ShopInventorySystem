d82441a712869af2a109667eecacfaf483941d64
gysherogney <>
2023-09-25 14:32:47 +0300

    Update admin.blade.php

--- HappieInvetory/resources/views/admin.blade.php
+++ HappieInvetory/resources/views/admin.blade.php
@@ -1,12 +1,105 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    adminniiii
-</body>
-</html>
+@extends('app')
+
+@section('title')
+
+    Admin
+    
+@endsection 
+
+@section('content')
+
+    <div class="container d-flex justify-content-center flex-wrap" style="padding-top: 8%;">
+        <div class="card bg-danger-subtle m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title"><b>12,456</b></h2>
+                <h5 class="mt-3">Products</h5>
+            </div>
+        </div>
+        <div class="card bg-primary m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title"><b>12,456</b></h2>
+                <h5 class="mt-3">Products</h5>
+            </div>
+        </div>
+        <div class="card bg-info-subtle m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title"><b>12,456</b></h2>
+                <h5 class="mt-3">Products</h5>
+            </div>
+        </div>
+        <div class="card bg-secondary m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title"><b>12,456</b></h2>
+                <h5 class="mt-3">Products</h5>
+            </div>
+        </div>
+        <div class="card bg-danger m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title"><b>12,456</b></h2>
+                <h5 class="mt-3">Products</h5>
+            </div>
+        </div>
+        <div class="card bg-success m-3" style="width: 18rem;">
+            <div class="card-body">
+                <h2 class="card-title"><b>12,456</b></h2>
+                <h5 class="mt-3">Products</h5>
+            </div>
+        </div>
+    </div>
+    <div class="container d-flex mt-5 pt-5" style="border-top: 4px solid black; max-width: 80%;">
+        <div class="container">
+             <h1>Statistic</h1>
+            <h6>Costomer/Day</h6> 
+            <img src="{{ asset('asset/image/imaes.png')}}" alt="" style=" width: 30vw; height: 50vh;">
+        </div>
+        <div class="container">
+            <h3>Top sell</h3>
+            <table class="table table-success table-striped">
+                            <thead>
+                    <tr>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <footer classstyle=" height: 10vh;">
+
+    </footer>
+    <style>
+        tr {
+            height: 55px;
+        }
+    </style>
+
+@endsection
